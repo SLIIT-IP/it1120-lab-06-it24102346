0933fa42251d50a1315d305a388ebd69d8d304a5
it24102346 <it24102346@my.sliit.lk>
2024-08-31 06:37:28 +0530

    IT24102346 Q1

---
+++ Q1/IT24102346Lab6Q1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class IT24102346Lab6Q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();
+
+        double square = number * number;
+        double squareRoot = Math.sqrt(number);
+
+        System.out.println("Square of " + number + " is: " + square);
+        System.out.println("Square root of " + number + " is: " + squareRoot);
+    }
+}
